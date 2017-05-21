3c22e388ed8ad5e05e2dffb72b8e14b158d9fca6
Kevin Faro <kevin.faro@gmail.com>
2017-05-21 14:09:02 -0400

    live

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="intelligence through emoji">
+    <meta name="author" content="kevin faro">
     <link rel="icon" href="/images/emoji/no_mouth.png">
 
-    <title>Emojint</title>
+    <title>emojint</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
